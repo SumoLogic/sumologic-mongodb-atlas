7e4a04b33c9c26e0290240f4c6d007f29f64965b
Kumar Prateek <kumar.prateek@sumologic.com>
2024-05-24 18:11:08 +0530

    Added requests to the requirements and tested with python version 3.8, 3.9, 3.10 and 3.11

--- setup.py
+++ setup.py
@@ -45,6 +45,10 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Operating System :: OS Independent'
     ],
